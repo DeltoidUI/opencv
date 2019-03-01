7525c2b48548a7590253634a247eeffcb7ea6416
rerixo <rerixo@gmail.com>
2019-03-01 20:28:25 +0000

    8

---
+++ 8.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('video/tekken.mp4')
+
+while True: 
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([20,0,0])
+    upper_red = np.array([255,255,255])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    kernel = np.ones((15,15), np.float32)/225
+    smoothed = cv2.filter2D(res, -1, kernel)
+    blur = cv2.GaussianBlur(res, (15,15), 0)
+    median = cv2.medianBlur(res,15)
+    bilateral = cv2.bilateralFilter(res,15,75,75)
+
+
+    #cv2.imshow('frame', frame)
+    #v2.imshow('mask', mask)
+    #cv2.imshow('res', res)
+    #cv2.imshow('smoothed', smoothed)
+    #cv2.imshow('blur', blur)
+    cv2.imshow('Median Blur',median)
+    #cv2.imshow('bilateral Blur',bilateral)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
